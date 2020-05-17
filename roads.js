ab895dad40fd5c136c3e6180dffb563552b18791
presanth s nair <presanthsnair2000@gmail.com>
2020-05-17 11:21:09 +0530

    Final file

---
+++ roads.js
@@ -0,0 +1,14 @@
+function display(){
+    var text=0
+    text=document.getElementById("feedback").value
+    if(text==0){
+        alert("please enter your comments")
+    }
+    else{
+        alert("Thanks for your comments")
+    }
+    
+}
+function upcoming(){
+    alert("sorry The series is upcoming ")
+}
